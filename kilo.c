4c4ed784ea283fdef6e8f4fbd8db792f6dc9bc73
AndrewG76 <aggravino@cpp.edu>
2021-12-02 19:53:47 +0000

    Finish step 7-12 on Entering Raw Mode

--- kilo.c
+++ kilo.c
@@ -2,6 +2,8 @@
 //CS2600, Final Part 2
 //12-10-21
 
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -15,12 +17,10 @@ void disableRawMode() {
 void enableRawMode() {
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
-    
-    struct termios raw;
 
-    tcgetattr(STDIN_FILENO, &raw);
-
-    raw.c_lflag &= ~(ECHO);
+    struct termios raw = orig_termios;
+    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -29,6 +29,13 @@ int main() {
     enableRawMode();
 
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
+
     return 0;
 }
